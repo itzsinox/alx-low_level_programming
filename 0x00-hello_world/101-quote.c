84f88f1030bb97949e138d92cac3d885cd4fab7f
itzsinox <jabar.yassine10@gmail.com>
2023-03-16 19:05:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line without using puts pr printf
+ *main - A program that prints a line without using puts pr printf
  *Return: 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is usful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is usful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
