9812a91586b63944fdbb3f184b298a73cab86238
itzsinox <jabar.yassine10@gmail.com>
2023-03-16 17:54:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with the function puts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle \n");
+  return (0);
+}
+  
