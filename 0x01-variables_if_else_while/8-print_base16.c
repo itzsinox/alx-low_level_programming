74e96227d1c8386b1f70a4a0e6bb996ba1176f6f
itzsinox <jabar.yassine10@gmail.com>
2023-03-17 17:58:16 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a prog that print 0..9 and a..f
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
