d706ceba563292c3e6e47e694f20df8381987030
itzsinox <jabar.yassine10@gmail.com>
2023-03-21 19:40:00 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - program that adds 2 integers and print result
+ * @i: int to be added to j
+ * @j: int to be added to i
+ * Return: sum of i and j
+ */
+int add(int i, int j)
+{
+	int c;
+
+	c = i + j;
+	return (c);
+}
