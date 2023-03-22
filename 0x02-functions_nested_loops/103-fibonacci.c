d8e4066eed0d43c9a43f3b16aced570bdae3f35a
itzsinox <jabar.yassine10@gmail.com>
2023-03-21 20:40:29 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - even liber abbaci
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int j, k, n, s;
+
+	j = 1;
+	k = 2;
+	s = 0;
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			s = s + j;
+		}
+		n = j + k;
+		j = k;
+		k = n;
+	}
+	printf("%lu\n", s);
+	return (0);
+}
