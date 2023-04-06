d3c989342c2ac4fd4de69acefeffd069767b7942
itzsinox <jabar.yassine10@gmail.com>
2023-04-05 17:14:52 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,7 @@
 /**
  * _sqrt_recursion - returns the natural sqrt of a num
  * @n: input
+ * @val: input
  * Return: the natural sqrt of num
  */
 int square(int n, int val);
@@ -9,6 +10,12 @@ int _sqrt_recursion(int n)
 {
 	return (square(n, 1));
 }
+/**
+ * square - return the square of num
+ * @n: input
+ * @val: input
+ * Return: square of num
+ */
 int square(int n, int val)
 {
 	if (val * val == n)
