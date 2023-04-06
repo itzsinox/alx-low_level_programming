2c0e241f90da4b7acc85a8de2bc63f4b8cf9b2b8
itzsinox <jabar.yassine10@gmail.com>
2023-04-05 19:16:37 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,26 +1,13 @@
 #include "main.h"
+
+int last_index(char *s);
+int check(char *s, int start, int end, int mod);
+
 /**
- * is_palindrome - checks if a string is palindrome
- * @s: string to check
- * Return: 1 if true and 0 otherwise
- */
-/**
- * last_index - returns the last char of a string
+ * last_index - cheks the last index
  * @s: string pointer
  * Return: int
  */
-int last_index(char *s);
-
-/**
- * check - checks for the palindrome
- * @s: string
- * @start: int moves from right to left
- * @end: int moves from left to right
- * @mod: int
- * Return: 1 if parlindrome true and 0 otherwise
- */
-int check(char *s, int start, int end, int mod);
-
 int last_index(char *s)
 {
 	int n = 0;
@@ -29,14 +16,25 @@ int last_index(char *s)
 		n += last_index(s + 1) + 1;
 	return (n);
 }
-
+/**
+ * is_palindrome - checks if a string is palindrome
+ * @s: string to check
+ * Return: 1 if true and 0 otherwise
+ */
 int is_palindrome(char *s)
 {
 	int end = last_index(s);
 
 	return (check(s, 0, end - 1, end % 2));
 }
-
+/**
+ * check - checks if the last and the first char are symetric
+ * @s: string
+ * @start: int moves from right to left
+ * @end: int moves from left to right
+ * @mod: int
+ * Return: 1 if true and 0 otherwise
+ */
 int check(char *s, int start, int end, int mod)
 {
 	if ((start == end && mod != 0) || (start == end + 1 && mod == 0))
