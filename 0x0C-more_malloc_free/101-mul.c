c6710d0613ef7eede1b7b2c184934b7fbfec718d
itzsinox <jabar.yassine10@gmail.com>
2023-04-13 17:26:39 -0700

    0x0c-more-malloc0free advanced

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
+		error();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -5,7 +5,7 @@
  * Return: 1 on success
  * and on error -1 is returned, and errno set appropriately
  */
-int _putchar(char c);
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
