7d099709c04502a92f73a362c2bb176517190ea6
itzsinox <jabar.yassine10@gmail.com>
2023-04-19 15:30:38 -0700

    0x0f

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print name using ptr to func
+ * @name: str to add
+ * @f: ptr to func
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - returns the sum of 2 numb
+ * @a: the first numb
+ * @b: the second numb
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sun - return the diff of 2 numb
+ * @a: first numb
+ * @b: second numb
+ * Return: the diff
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of 2 numb
+ * @a: first numb
+ * @b: second numb
+ * Return: the value of a*b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the division of 2 numb
+ * @a: first numb
+ * @b: second numb
+ * Return: the div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the modulo of 2 numb
+ * @a: first numb
+ * @b: second numb
+ * Return: the mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
