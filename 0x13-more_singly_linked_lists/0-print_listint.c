bf527375a0a19d3eab2071ab0ff0f0e7d6a7aa85
itzsinox <jabar.yassine10@gmail.com>
2023-05-02 12:33:57 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prt elmt 
+ * print_listint - prt elmt
  * @h: print lkd-ls
  * Return: nodes
  */
@@ -9,11 +9,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t a = 0;
 
-	do
+	while (a)
 	{
 		printf("%d\n", h->n);
 		a++;
 		h = h->next;
-	} while (h)
+	}
 	return (a);
 }
